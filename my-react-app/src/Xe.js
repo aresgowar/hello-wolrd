fd9e8928f4adbfc99890f0772603a6282a0044ce
aresgowar <ntd31101999@gmail.com>
2022-03-12 08:13:18 +0700

    1

--- my-react-app/src/Xe.js
+++ my-react-app/src/Xe.js
@@ -5,7 +5,6 @@ import React from 'react';
 class Xe extends React.Component {
 
   render() {
-    console.log('Xe class ', this.props.color)
     return <h1 style={{ color: this.props.color }}>{this.props.value} {this.props.branch}</h1>
   }
 }
